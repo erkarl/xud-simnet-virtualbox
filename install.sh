198dfe8dbe9d88da4c3308dc71482af937a75f3b
Karl Ranna <karl@karlranna.com>
2018-11-05 22:08:35 +0200

    Clone Github repo within install

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 INSTALL_DIR=~/xud-deploy
+GITHUB_REPO=https://github.com/erkarl/xud-deploy-virtualbox
 echo "Downloading xud-deploy to $INSTALL_DIR"
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR || exit
+git clone $GITHUB_REPO $INSTALL_DIR
