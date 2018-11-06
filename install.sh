b06e0530ce17fbf78db34e56ab917c04448f6b61
Karl Ranna <karl@karlranna.com>
2018-11-06 14:15:06 +0200

    Update install path

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ then
   echo "Virtualbox is not installed."
   exit 1
 fi
-INSTALL_DIR=~/xud-deploy
-GITHUB_REPO=https://github.com/erkarl/xud-deploy-virtualbox
-echo "Downloading xud-deploy to $INSTALL_DIR"
+INSTALL_DIR=~/xud-simnet-virtualbox
+GITHUB_REPO=https://github.com/erkarl/xud-simnet-virtualbox
+echo "Downloading xud-simnet-virtualbox to $INSTALL_DIR"
 mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR || exit
 git clone $GITHUB_REPO $INSTALL_DIR
